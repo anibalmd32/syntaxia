ddb5fc798c4d0846992f07082880fcee636dc565
Anibal Mendoza <anibalmd32@gmail.com>
2025-11-25 11:53:50 -0400

    refactor generateUniqueKey function

--- src/utils/generateUniqueKey.ts
+++ src/utils/generateUniqueKey.ts
@@ -1,14 +1,3 @@
 export function generateUniqueKey(): string {
-  const randomPart = Math.random().toString(36).substring(2, 9);
-  const timestampPart = Date.now().toString(36);
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  const randomChars = Array.from(
-    {
-      length: 4,
-    },
-    () => characters[Math.floor(Math.random() * characters.length)],
-  ).join("");
-
-  return `key-${timestampPart}-${randomPart}-${randomChars}`;
+  return crypto.randomUUID();
 }
